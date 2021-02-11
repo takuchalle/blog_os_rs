7a3d92df58e953d42890e4a5b1a3ecde0f3db613
takuchalle <me@takuchalle.dev>
2021-02-11 23:24:10 +0900

    feat: switch to supervisor

--- boot.s
+++ boot.s
@@ -3,12 +3,17 @@
 .section .boot, "ax", @progbits
 
 .global _boot
+.global trap_entry
 
 _boot:
 	la	sp, stack + 1024
 	j	_start
 
+trap_entry:
+	j	_main
+
 .bss
 
 stack:
 	.skip 1024
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
 
+use blog_os_rs::csr;
 use blog_os_rs::println;
 
 use core::panic::PanicInfo;
+
+extern "C" {
+    static trap_entry: u8;
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -11,8 +17,23 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[no_mangle]
+pub extern "C" fn _main() -> ! {
+    println!("main");
+    loop {}
+}
+
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let x = 3;
-    println!("Hello, World! {}", x);
+    let mut mstatus = csr::mstatus::Mstatus::read();
+    mstatus.set_mpp(csr::mstatus::Mpp::Super);
+    mstatus.write();
+
+    let addr = unsafe { (&trap_entry as *const u8) } as usize;
+    println!("addr {:X}", addr);
+    csr::mepc::Mepc::write(addr);
+
+    //
+    unsafe { asm!("mret") }
+
     loop {}
 }
